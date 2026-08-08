aaa8dabad6effbd307af183bef38f695769896ab
sanchezes2024-eng <sanchezes2024@gmail.com>
2026-08-08 19:14:35 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 const { Server } = require("socket.io");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
     let filePath;
